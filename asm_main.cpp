4300777d21b751862d60622f160176a6c2ebf9e7
KaitmazianMO <70343770+KaitmazianMO@users.noreply.github.com>
2021-03-07 00:16:06 +0300

    [input processing]

--- asm_main.cpp
+++ asm_main.cpp
@@ -7,21 +7,28 @@
  
 //g++ -Wall asm_main.cpp assembler.h assembler.cpp comands.h label.h label.cpp buffer.h  buffer.cpp errors.h byte_code.h byte_code.cpp listing.h listing.cpp -o assembler
 int main (int argc, char *argv[]) 
-    {                     
+    {                   
+    if (argc != 4)
+        {
+        printf ("Wrong input format. The format is <sourceFile> <binaryFile> <lisitngFile>\n");
+        return !0;
+        }
+
     printf ("start assembling...\n"); 
 
     Assembler *asm_ptr = newAssembler (argv [3]);                         //           src       bin       lst
     NEW_ASSEMBLER_LISTING_BLOCK ("%d, \"%s\", \"%s\", \"%s\", \"%s\"", argc, argv [0], argv [1], argv [2], argv [3])
 
-    translateFile (asm_ptr, argv [1]);          
+    int err = translateFile (asm_ptr, argv [1]);          
 
-    writeByteCode (asm_ptr, argv [2]);       
+    if (!err)
+        writeByteCode (asm_ptr, argv [2]);       
 
     ASSEMBLER_BLOCK_PREMATURE_COMPLETION()
     asm_ptr = dellAssembler (asm_ptr);
     printf ("finish assembling\n");    
-   
-    return 0;
+
+    return err;
     }
  
 
