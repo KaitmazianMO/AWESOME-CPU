70af2182b0ab0855fc0aba7eac3b9b2933aeae2c
KaitmazianMO <70343770+KaitmazianMO@users.noreply.github.com>
2021-03-09 13:14:14 +0300

    [refactor]

--- cpu_main.cpp
+++ cpu_main.cpp
@@ -2,19 +2,16 @@
 #include "CPU.h"
 
 
-// g++ -fpermissive cpu_main.cpp CPU.h CPU.cpp assembler.h comands.h buffer.h label.h label.cpp buffer.cpp errors.h byte_code.h byte_code.cpp stack.h -o CPU
+//  g++ -fpermissive cpu_main.cpp CPU.h CPU.cpp assembler.h comands.h buffer.h label.h label.cpp text.h text.cpp errors.h byte_code.h byte_code.cpp listing.h listing.cpp stack.h -o TheCPU
 int main(int argc, char *argv[]) {
     printf ("CPU start..\n");
     CPU cpu = {};
     CPUctor (&cpu, argv[1]);
-    
-    for (int i = 0; i < 114; ++i) {
-        printf ("[%2d]IP = %d\n", i, cpu.bcode.data [i]);
-    }
+
     
     int err = CPURun (&cpu);
 
-    //CPUdtor (&cpu);   if not already deleted
+    CPUdtor (&cpu);
     printf ("CPU finish(%d)\n", err);
     return err;
 }
